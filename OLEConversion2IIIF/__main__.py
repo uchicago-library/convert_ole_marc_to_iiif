33facf15ea20f19b720d72cdca51bed164aa34a2
verbalhanglider <tyler@danstrom.com>
2018-05-28 09:41:35 -0500

    implemented custom action ExtractIIIFIMageLinks

--- OLEConversion2IIIF/__main__.py
+++ OLEConversion2IIIF/__main__.py
@@ -1,7 +1,30 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Action, ArgumentError
+from os.path import exists
+import requests
 
 __version__ = "0.0.1"
 __author__ = "Tyler Danstrom"
+
+class ExtractIIIFImageLinks(Action):
+    """a custom action class to process the manifest.txt file list of IIIF links, check them for validity and reutrn a list of the links
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super(ExtractIIIFImageLinks, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, args, values, options_string=None):
+        if not exists(values):
+            raise ArgumentError(self, "value for --image-manifest does not exist on disk")
+        imgs = []
+        with open(values, 'r', ) as read_file:
+            lines = read_file.readlines()
+            for line in lines:
+                if not requests.get(line, 'head').status_code == 200:
+                    msg = "{} in {} is not a valid IIIF image url".format(line, dest)
+                    raise ArgumentError(self, msg)
+                else:       
+                    imgs.append(line)
+        setattr(args, self.dest, imgs)
+
 
 def main():
     try:
